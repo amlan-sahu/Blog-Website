7e9690a39ea74939b4960abb85e243b2f5cc8486
amlan-sahu <amlan9531@gmail.com>
2024-12-31 02:36:40 +0530

    refactor: remove inline TinyMCE script from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -77,7 +77,7 @@ const router = createBrowserRouter([
     },
 ])
 
-<script type="module"> import { loadTinyMCE } from './src/loadTinyMCE.js'; loadTinyMCE(); </script>
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
